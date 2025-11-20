b605500811b4d6e3c90ff244748c6951afe15c50
k0imburi <pablokahura@gmail.com>
2025-11-20 11:14:06 +0300

    Fix: Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this to '/repo-name/' if deploying to https://username.github.io/repo-name/
+  base: '/portfolio/', 
 })
